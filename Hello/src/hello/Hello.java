540c985a6f591df9efa53e39879adb6787096650
Ankur480 <ankurmondal039@gmail.com>
2026-04-27 07:14:39 +0530

    Initial commit

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,10 @@
+package hello;
+public class Hello {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello! This is my first java program.");
+             
+    }
+    
+}
