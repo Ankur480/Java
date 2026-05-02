646d4a948a508cb0ac9987198a689c7ba84fbf87
Ankur480 <ankurmondal039@gmail.com>
2026-05-02 20:18:19 +0530

    Add code

---
+++ armstrong/src/armstrong/Armstrong.java
@@ -0,0 +1,26 @@
+package armstrong;
+import java.lang.*;
+import java.util.*;
+public class Armstrong {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
+        
+        int temp=num;
+        double sum=0;
+        double rem;
+        while (temp!=0)
+        {
+            rem = temp % 10;
+            sum = sum + Math.pow(rem, 3);
+            temp = temp/10;
+        }
+        
+        if(num==sum)
+            System.out.println("The given number is armstrong number.");
+    }
+    
+}
