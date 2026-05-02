03ddb5ab176e7cede87e1c06a7173fd56ee88aba
Ankur480 <ankurmondal039@gmail.com>
2026-05-02 17:29:56 +0530

    Practice new programs

---
+++ menuprogram/src/menuprogram/Menuprogram.java
@@ -0,0 +1,42 @@
+//menu driven program
+
+package menuprogram;
+import java.util.*;
+
+public class Menuprogram {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Menu");
+        System.out.println("====");
+        System.out.println("ADD");
+        System.out.println("SUB");
+        System.out.println("MUL");
+        System.out.println("DIV");
+        
+        System.out.println("Enter 2 Numbers");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        sc.nextInt();
+        System.out.println("Enter Option in words ");
+        String option=sc.nextLine();
+        option=option.toUpperCase();
+        
+        switch(option)
+        {
+            case "ADD": System.out.println("Sum is "+(x+y));
+            break;
+            case "SUB": System.out.println("Subraction is "+(x-y));
+            break;
+            case "MUL": System.out.println("Mul is "+(x*y));
+            break;
+            case "DIV": System.out.println("Divison is "+(x/y));
+            break;
+            default: System.out.println("Invalid Option");
+            break;
+        }
+
+    }
+    
+}
