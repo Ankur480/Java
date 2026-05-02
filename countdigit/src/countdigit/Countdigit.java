88289258ef99d3f4e8d1e9673c8dde277631ba33
Ankur480 <ankurmondal039@gmail.com>
2026-05-02 20:06:54 +0530

    Add new code

--- countdigit/src/countdigit/Countdigit.java
+++ countdigit/src/countdigit/Countdigit.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package countdigit;
-
-/**
- *
- * @author ankur
- */
+import java.util.*;
 public class Countdigit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+        int i=0, r;
+        int temp=num;
+        while(temp!=0){
+            r = temp%10;
+            temp=temp/10;
+            i++;
+        }
+        System.out.println("Number of digit in the number "+num+" is "+i);
     }
     
 }
